8abd90b187d5d7226da65b1d5892e37528bffcbc
SandeepThakare <sandythakare9@gmail.com>
2018-05-17 18:13:23 +0530

    serverless-offline with testcases using mocha

--- testcases/customer-test-cases/getCustomer.test.js
+++ testcases/customer-test-cases/getCustomer.test.js
@@ -1,10 +1,4 @@
-// require('dotenv').load();
 const request = require('supertest');
-// var expect = require('chai').expect;
-// var LambdaTester = require('lambda-tester');
-// var getCustomerInfo = require('../../customerInfo/getCustomerInfo').getCustomerInfo;
-// var assert = require('chai').assert;
-// var should = require('chai').should();
 
 describe('getCustomerInfo test', function () {
 
@@ -17,47 +11,4 @@ describe('getCustomerInfo test', function () {
 			.expect('Content-Type', /json/)
 			.expect(200, { email: 'sandythakare9@gmail.com'}, done);
 	});
-
-	// it('successful invocation: getCustomer function', function (done) {
-	// 	var event = {
-	// 		'pathParameters': {'email':'sandythakare9@gmail.com'}
-	// 	};
-	// 	var context = {};
-	// 	var callback =(ctx, data) => {
-	// 		try{
-	// 			console.log('test data -', data);
-	// 			// console.log(data.body.message, data.statusCode);
-	// 			assert(data);
-	// 			assert(data.statusCode == 200);
-	// 			done();
-	// 		}
-	// 		catch (err){
-	// 			console.log('error', err);
-	// 			done(err);
-	// 		}
-	// 	};
-	// 	getCustomerInfo(event, context, callback);
-	// });
-
-	// it('successful invocation: getCustomer 2 function', function (done) {
-	// 	var event = {
-	// 		'pathParameters': {'email':'sandythakare9'}
-	// 	};
-	// 	var context = {};
-	// 	var callback =(ctx, data) => {
-	// 		try{
-	// 			// console.log('test data -', data);
-	// 			// console.log(JSON.parse(data.body), data.statusCode);
-	// 			// let bodydata = JSON.parse(data.body);
-	// 			// assert(data.body === 'Invalid Email');
-	// 			assert(data.statusCode == 406);
-	// 			done();
-	// 		}
-	// 		catch (err){
-	// 			console.log('error', err);
-	// 			done(err);
-	// 		}
-	// 	};
-	// 	getCustomerInfo(event, context, callback);
-	// });
 });
