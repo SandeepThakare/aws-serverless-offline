3853906bf73367af884a72e259144367e26af746
SandeepThakare <sandythakare9@gmail.com>
2018-06-05 12:11:13 +0530

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
-var path = require('path');
+
 module.exports = {
 	entry: slsw.lib.entries,
-	externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
 	target: 'node',
+	// Generate sourcemaps for proper error messages
+	devtool: 'source-map',
+	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+	optimization: {
+		// We no not want to minimize our code.
+		minimize: false
+	},
+	performance: {
+		// Turn off size warnings for entry points
+		hints: false
+	},
+	// Since 'aws-sdk' is not compatible with webpack,
+	// we exclude all node dependencies
+	externals: [nodeExternals()],
+	// Run babel on all .js files and skip those in node_modules
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
-				exclude: /node_modules/, // in order to ignore built-in modules like path, fs, etc.
-				loaders: ['babel-loader'],
+				loader: 'babel-loader',
 				include: __dirname,
-			},
-		],
-	},
-	output: {
-		libraryTarget: 'commonjs',
-		path: path.join(__dirname, '.webpack'),
-		filename: '[name].js',
-	},
+				exclude: /node_modules/
+			}
+		]
+	}
 };
+
